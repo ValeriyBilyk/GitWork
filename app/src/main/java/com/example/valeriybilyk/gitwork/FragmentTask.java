23fc149b0e43e15f7a605ae794908ffc20d2b6ae
Alex Kucherov <a.kucherov@mozidev.com>
2016-07-27 12:20:09 +0300

    fragment update

--- app/src/main/java/com/example/valeriybilyk/gitwork/FragmentTask.java
+++ app/src/main/java/com/example/valeriybilyk/gitwork/FragmentTask.java
@@ -1,7 +1,15 @@
 package com.example.valeriybilyk.gitwork;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.View;
 
 public class FragmentTask extends Fragment {
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
