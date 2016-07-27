966a2873d5f32fe59964540c0e7320f2a13708a0
ValeriyBilyk <bilyk1996@gmail.com>
2016-07-27 11:52:25 +0300

    2 task

--- app/src/main/java/com/example/valeriybilyk/gitwork/MainActivity.java
+++ app/src/main/java/com/example/valeriybilyk/gitwork/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.valeriybilyk.gitwork;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ActionMode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
+    }
+
+    @Nullable
+    @Override
+    public ActionMode onWindowStartingActionMode(ActionMode.Callback callback) {
+        return super.onWindowStartingActionMode(callback);
     }
 }
